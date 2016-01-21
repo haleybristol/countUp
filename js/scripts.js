b16a5ea09917f542a400da40a7f6d37fde1bdef0
Haley Bristol and Nathan Otto <hebristol@gmail.com>
2016-01-21 10:44:38 -0800

    add interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,19 @@ var countBy = function(countTo, multiplyTo) {
     console.log(i);
   }
   console.log(i);
-  return i;
+  var output = i;
+  return output;
 }
+
+$(document).ready(function() {
+  $("form").submit(function(event){
+    var countTo = parseInt($("input#countTo").val());
+    var multiplyTo = parseInt($("input#multiplyTo").val());
+    var result = countBy(countTo, multiplyTo);
+    console.log("Test", result);
+    // $(".pig-latin").text(word);
+    $("#numbers").append(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
